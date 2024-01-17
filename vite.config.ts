0157b4c3237b53c08e8cb835edc5f81fd7cdab57
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-17 19:08:06 +1100

    build: add vite-tsconfig-paths
    Easy absolute imports.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [tsconfigPaths(), react(), dts({ include: ['lib'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
