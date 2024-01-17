c36b7890f7e2d437ea019a056171f4465a6b12f1
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-17 19:07:09 +1100

    build: remove chakra storybook addon
    It wasn't doing anything. Dunno.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@chakra-ui/storybook-addon',
   ],
   framework: {
     name: '@storybook/react-vite',
@@ -18,9 +17,6 @@ const config: StorybookConfig = {
   },
   core: {
     disableTelemetry: true,
-  },
-  features: {
-    emotionAlias: false,
   },
 };
 export default config;
