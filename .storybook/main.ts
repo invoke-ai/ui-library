226a9360ce0dbbb5e6392bddb3248e759a476238
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-17 18:39:10 +1100

    feat: set up storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@chakra-ui/storybook-addon',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  features: {
+    emotionAlias: false,
+  },
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from '@storybook/react';
+
+export const parameters = {
+  chakra: {
+    // theme,
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
