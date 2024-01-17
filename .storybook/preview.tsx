76f7b111f1a08cd13e97405152280b34454ec0d5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-17 19:11:07 +1100

    feat(sb): set up storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,6 @@
 import type { Preview } from '@storybook/react';
-
-export const parameters = {
-  chakra: {
-    // theme,
-  },
-};
+import React from 'react';
+import { ChakraProvider, DarkMode } from '@chakra-ui/react';
 
 const preview: Preview = {
   parameters: {
@@ -16,6 +12,17 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => {
+      return (
+        <ChakraProvider>
+          <DarkMode>
+            <Story />
+          </DarkMode>
+        </ChakraProvider>
+      );
+    },
+  ],
 };
 
 export default preview;
