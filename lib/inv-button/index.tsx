67264f53534271fb3bf23379890c7f1ae98c5046
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-18 16:22:16 +1100

    feat: export icon button

--- lib/inv-button/index.tsx
+++ lib/inv-button/index.tsx
@@ -2,3 +2,5 @@ export type { InvButtonProps } from './inv-button';
 export { InvButton } from './inv-button';
 export type { InvButtonGroupProps } from './inv-button-group';
 export { InvButtonGroup } from './inv-button-group';
+export type { InvIconButtonProps } from './inv-icon-button';
+export { InvIconButton } from './inv-icon-button';
