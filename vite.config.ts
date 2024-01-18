293c2df36049d020de5057915c91b8a649057692
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-18 16:20:20 +1100

    build: do not bundle individual component themes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ exclude: ['**/*.stories.tsx'] })],
+  plugins: [react(), dts({ exclude: ['**/*.stories.tsx', 'lib/inv-*/theme.ts'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
