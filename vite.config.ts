f30a8072899b9749a33fead54fa74983faa568c9
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-18 15:04:56 +1100

    fix: reverts `vite-tsconfig-paths`, fixing path resolution in build
    `vite-tsconfig-paths` helps vite to resolve "absolute" paths when compiling.
    
    This plugin doesn't play nicely with `vite-plugin-dts`.
    
    This is a known problem, with GH issues in both repos.
    
    Another plugin `vite-plugin-lib`, which is based on `vite-plugin-dts`, apparently handles all of this, but I couldn't get it to work.
    
    WIth `vite-tsconfig-paths` being removed, we also have no need for `eslint-plugin-path`, because we are using relative paths, so it's also removed.
    
    Tweaked `tsconfig.json` and `vite.config.ts` in response to these changes.
    
    The packages now build correctly and the types work.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { glob } from 'glob';
-import { extname, relative, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), dts({ exclude: ['**/*.stories.tsx'] })],
+  plugins: [react(), dts({ exclude: ['**/*.stories.tsx'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
@@ -35,21 +32,6 @@ export default defineConfig({
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
       },
-      input: Object.fromEntries(
-        glob
-          .sync('lib/**/*.{ts,tsx}', {
-            // we don't want test files included
-            ignore: ['lib/**/*.spec.tsx', 'lib/__mocks__/*', 'lib/**/*.stories.*'],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative('lib', file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
     },
     sourcemap: true,
   },
