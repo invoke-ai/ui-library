00ece9f25ca6417a74d7b2d07f285cf4e63966db
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-18 18:17:41 +1100

    fix(theme): type `spinKeyframes`

--- lib/theme/animations.ts
+++ lib/theme/animations.ts
@@ -1,6 +1,7 @@
 import { keyframes } from '@chakra-ui/react';
+import type { Keyframes } from '@emotion/react';
 
-export const spinKeyframes = keyframes`
+export const spinKeyframes: Keyframes = keyframes`
   from {
     transform: rotate(0deg);
   }
