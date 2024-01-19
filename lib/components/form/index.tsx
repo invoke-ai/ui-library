f8ee9aa4dff523285cfdd7c79da6b8764e81ad0c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-19 18:25:19 +1100

    fix(components/form): export FormControlGroup

--- lib/components/form/index.tsx
+++ lib/components/form/index.tsx
@@ -1,5 +1,7 @@
 export type { FormControlProps } from './form-control';
 export { FormControl } from './form-control';
+export type { FormControlGroupProps } from './form-control-group';
+export { FormControlGroup } from './form-control-group';
 export type { FormLabelProps } from './form-label';
 export { FormLabel } from './form-label';
 export type { FormErrorMessageProps, FormHelperTextProps } from './wrapper';
