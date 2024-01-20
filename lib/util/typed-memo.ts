99b0b3c5c7b04f78b9b1a34a36fa3f0f9b77bfdf
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-20 19:25:00 +1100

    feat(util): improved types in `typedMemo`

--- lib/util/typed-memo.ts
+++ lib/util/typed-memo.ts
@@ -1,6 +1,11 @@
+import type React from 'react';
 import { memo } from 'react';
 
 /**
  * A typed version of React.memo, useful for components that take generics.
  */
-export const typedMemo: <T>(c: T) => T = memo;
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export const typedMemo: <T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>>(
+  component: T,
+  propsAreEqual?: (prevProps: React.ComponentProps<T>, nextProps: React.ComponentProps<T>) => boolean
+) => T & { displayName?: string } = memo;
