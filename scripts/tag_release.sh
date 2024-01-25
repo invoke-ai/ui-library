906e40b6cb4bd5e2e29e25faae3b3123dac1f77c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-25 21:18:17 +1100

    build: bail if tag already exists

--- scripts/tag_release.sh
+++ scripts/tag_release.sh
@@ -2,7 +2,12 @@
 
 # Parse version from package.json
 version=$(grep -oP '"version": "\K(.*?)(?=")' package.json)
-
 tag="v$version"
+
+if git rev-parse "$tag" >/dev/null 2>&1; then
+  echo "Tag ${tag} already exists. Exiting..."
+  exit 1
+fi
+
 git tag $tag
 git push origin $tag
