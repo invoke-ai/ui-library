b2747bfbd77afbdad40c44df74590369150a63c8
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-28 21:53:39 +1100

    feat(hooks): add docstrings for use-global-modifiers

--- lib/hooks/use-global-modifiers.ts
+++ lib/hooks/use-global-modifiers.ts
@@ -2,9 +2,25 @@ import { useStore } from '@nanostores/react';
 import { atom } from 'nanostores';
 import { useEffect } from 'react';
 
+/**
+ * A `nanostores` atom that represents the current state of the shift key.
+ * If true, the shift key is currently pressed.
+ */
 export const $shift = atom(false);
+/**
+ * A `nanostores` atom that represents the current state of the ctrl key.
+ * If true, the ctrl key is currently pressed.
+ */
 export const $ctrl = atom(false);
+/**
+ * A `nanostores` atom that represents the current state of the meta key.
+ * If true, the meta key is currently pressed.
+ */
 export const $meta = atom(false);
+/**
+ * A `nanostores` atom that represents the current state of the alt key.
+ * If true, the alt key is currently pressed.
+ */
 export const $alt = atom(false);
 
 const $isInitialized = atom(0);
@@ -35,15 +51,27 @@ export const useGlobalModifiersInit = () => {
   }, []);
 };
 
+/**
+ * Sets the state of the $shift atom.
+ */
 const setShift = (shift: boolean) => {
   $shift.set(shift);
 };
+/**
+ * Sets the state of the $ctrl atom.
+ */
 const setCtrl = (ctrl: boolean) => {
   $ctrl.set(ctrl);
 };
+/**
+ * Sets the state of the $alt atom.
+ */
 const setAlt = (alt: boolean) => {
   $alt.set(alt);
 };
+/**
+ * Sets the state of the $meta atom.
+ */
 const setMeta = (meta: boolean) => {
   $meta.set(meta);
 };
@@ -57,21 +85,37 @@ export const useGlobalModifiersImperativeAPI = () => {
   return { setShift, setCtrl, setAlt, setMeta };
 };
 
+/**
+ * Provides the current state of the global shift modifier.
+ * @returns true if the shift key is currently pressed.
+ */
 export const useShiftModifier = () => {
   const shift = useStore($shift);
   return shift;
 };
 
+/**
+ * Provides the current state of the global ctrl modifier.
+ * @returns true if the ctrl key is currently pressed.
+ */
 export const useCtrlModifier = () => {
   const ctrl = useStore($ctrl);
   return ctrl;
 };
 
+/**
+ * Provides the current state of the global alt modifier.
+ * @returns true if the alt key is currently pressed.
+ */
 export const useAltModifier = () => {
   const alt = useStore($alt);
   return alt;
 };
 
+/**
+ * Provides the current state of the global meta modifier.
+ * @returns true if the meta key is currently pressed.
+ */
 export const useMetaModifier = () => {
   const meta = useStore($meta);
   return meta;
