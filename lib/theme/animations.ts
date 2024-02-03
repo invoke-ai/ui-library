6bad4570db0be6728477541959837a482b6a870d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-03 21:28:17 +1100

    feat(theme): add spinWithPauseAnimation

--- lib/theme/animations.ts
+++ lib/theme/animations.ts
@@ -11,3 +11,18 @@ export const spinKeyframes: Keyframes = keyframes`
 `;
 
 export const spinAnimation = `${spinKeyframes} 1s linear infinite`;
+
+// Rotate 180deg, pause, rotate 180deg, pause, repeat
+export const spinWithPauseKeyframes: Keyframes = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  20%, 50% {
+    transform: rotate(180deg);
+  }
+  70%, 100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const spinWithPauseAnimation = `${spinWithPauseKeyframes} 4s ease infinite`;
