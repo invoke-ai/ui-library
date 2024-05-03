5d06493546c458eafee8b4c6048f6f6669e2fec0
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-03 10:57:11 +1000

    feat(tabs): re-export TabIndicator, TabIndicatorProps

--- lib/components/tabs/index.tsx
+++ lib/components/tabs/index.tsx
@@ -1,4 +1,4 @@
 export type { TabProps } from './tab';
 export { Tab } from './tab';
-export type { TabListProps, TabPanelProps, TabPanelsProps, TabsProps } from './wrapper';
-export { TabList, TabPanel, TabPanels, Tabs } from './wrapper';
+export type { TabIndicatorProps, TabListProps, TabPanelProps, TabPanelsProps, TabsProps } from './wrapper';
+export { TabIndicator, TabList, TabPanel, TabPanels, Tabs } from './wrapper';
