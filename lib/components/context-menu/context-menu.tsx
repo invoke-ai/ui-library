9cb4bd2181479c13a84330f720bf94cd2b2af0d8
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-10-04 18:13:59 +1000

    feat(context-menu): add long press config
    - Add `longPressBailThreshold`, when moved this distance while long-pressing, abort the long press
    - Add `withLongPress`, enables/disables long press

--- lib/components/context-menu/context-menu.tsx
+++ lib/components/context-menu/context-menu.tsx
@@ -18,135 +18,190 @@ export interface ContextMenuProps<T extends HTMLElement = HTMLDivElement> {
   menuButtonProps?: MenuButtonProps;
   stopPropagation?: boolean;
   stopImmediatePropagation?: boolean;
+  withLongPress?: boolean;
   longPressDelayMs?: number;
+  longPressBailThreshold?: number;
 }
 
-export const ContextMenu = typedMemo(<T extends HTMLElement = HTMLElement>(props: ContextMenuProps<T>) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [position, setPosition] = useState([-1, -1]);
-  const targetRef = useRef<T>(null);
-  const lastPositionRef = useRef([-1, -1]);
-  const longPressTimeoutRef = useRef(0);
-  const animationTimeoutRef = useRef(0);
+export const ContextMenu = typedMemo(
+  <T extends HTMLElement = HTMLElement>({
+    withLongPress = true,
+    longPressDelayMs = 500,
+    longPressBailThreshold = 10,
+    renderMenu,
+    children,
+    menuProps,
+    portalProps,
+    menuButtonProps,
+    stopPropagation = false,
+    stopImmediatePropagation = false,
+  }: ContextMenuProps<T>) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [position, setPosition] = useState([-1, -1]);
+    const targetRef = useRef<T>(null);
+    const lastPositionRef = useRef<[number, number]>([-1, -1]);
+    const longPressTimeoutRef = useRef(0);
+    const animationTimeoutRef = useRef(0);
 
-  useGlobalMenuClose(onClose);
+    useGlobalMenuClose(onClose);
 
-  const onContextMenu = useCallback(
-    (e: MouseEvent | PointerEvent) => {
-      if (e.shiftKey) {
-        onClose();
-        return;
-      }
-      // `contains()` requires the arg to be Node. Technically it can be any EventTarget, but it won't cause an error.
-      /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions */
-      if (targetRef.current?.contains(e.target as Node) || e.target === targetRef.current) {
-        if (props.stopImmediatePropagation) {
-          e.stopImmediatePropagation();
-        }
-        if (props.stopPropagation) {
-          e.stopPropagation();
-        }
-        // clear pending delayed open
-        window.clearTimeout(animationTimeoutRef.current);
-        e.preventDefault();
-        if (lastPositionRef.current[0] !== e.pageX || lastPositionRef.current[1] !== e.pageY) {
-          // if the mouse moved, we need to close, wait for animation and reopen the menu at the new position
+    const onContextMenu = useCallback(
+      (e: MouseEvent | PointerEvent) => {
+        if (e.shiftKey) {
           onClose();
-          animationTimeoutRef.current = window.setTimeout(() => {
+          return;
+        }
+        // `contains()` requires the arg to be Node. Technically it can be any EventTarget, but it won't cause an error.
+        /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions */
+        if (targetRef.current?.contains(e.target as Node) || e.target === targetRef.current) {
+          if (stopImmediatePropagation) {
+            e.stopImmediatePropagation();
+          }
+          if (stopPropagation) {
+            e.stopPropagation();
+          }
+          // clear pending delayed open
+          window.clearTimeout(animationTimeoutRef.current);
+          e.preventDefault();
+          if (lastPositionRef.current[0] !== e.pageX || lastPositionRef.current[1] !== e.pageY) {
+            // if the mouse moved, we need to close, wait for animation and reopen the menu at the new position
+            onClose();
+            animationTimeoutRef.current = window.setTimeout(() => {
+              onOpen();
+              setPosition([e.pageX, e.pageY]);
+            }, 100);
+          } else {
+            // else we can just open the menu at the current position
             onOpen();
             setPosition([e.pageX, e.pageY]);
-          }, 100);
-        } else {
-          // else we can just open the menu at the current position
-          onOpen();
-          setPosition([e.pageX, e.pageY]);
+          }
         }
-      }
-      lastPositionRef.current = [e.pageX, e.pageY];
-    },
-    [onClose, onOpen, props.stopImmediatePropagation, props.stopPropagation]
-  );
+        lastPositionRef.current = [e.pageX, e.pageY];
+      },
+      [onClose, onOpen, stopImmediatePropagation, stopPropagation]
+    );
 
-  // Use a long press to open the context menu on touch devices
-  const onPointerDown = useCallback(
-    (e: PointerEvent) => {
-      if (e.pointerType === 'mouse') {
-        // Bail out if it's a mouse event - this is for touch/pen only
+    // Use a long press to open the context menu on touch devices
+    const onPointerDown = useCallback(
+      (e: PointerEvent) => {
+        if (!withLongPress) {
+          return;
+        }
+
+        if (e.pointerType === 'mouse') {
+          // Bail out if it's a mouse event - this is for touch/pen only
+          return;
+        }
+        longPressTimeoutRef.current = window.setTimeout(() => {
+          onContextMenu(e);
+        }, longPressDelayMs ?? 500); // Adjust the delay as needed
+
+        lastPositionRef.current = [e.pageX, e.pageY];
+      },
+      [withLongPress, longPressDelayMs, onContextMenu]
+    );
+
+    const onPointerMove = useCallback(
+      (e: PointerEvent) => {
+        if (!withLongPress) {
+          return;
+        }
+
+        if (longPressTimeoutRef.current === null) {
+          return;
+        }
+
+        const lastPosition = lastPositionRef.current;
+
+        const distanceFromLastPosition = Math.hypot(e.pageX - lastPosition[0], e.pageY - lastPosition[1]);
+
+        if (distanceFromLastPosition > longPressBailThreshold) {
+          clearTimeout(longPressTimeoutRef.current);
+        }
+      },
+      [longPressBailThreshold, withLongPress]
+    );
+
+    const onPointerUp = useCallback(() => {
+      if (!withLongPress) {
         return;
       }
-      longPressTimeoutRef.current = window.setTimeout(() => {
-        onContextMenu(e);
-      }, props.longPressDelayMs ?? 500); // Adjust the delay as needed
-    },
-    [onContextMenu, props.longPressDelayMs]
-  );
 
-  const onPointerUp = useCallback(() => {
-    if (longPressTimeoutRef.current) {
-      clearTimeout(longPressTimeoutRef.current);
-    }
-  }, []);
+      if (longPressTimeoutRef.current) {
+        clearTimeout(longPressTimeoutRef.current);
+      }
+    }, [withLongPress]);
 
-  const onPointerCancel = useCallback(() => {
-    if (longPressTimeoutRef.current) {
-      clearTimeout(longPressTimeoutRef.current);
-    }
-  }, []);
+    const onPointerCancel = useCallback(() => {
+      if (!withLongPress) {
+        return;
+      }
 
-  useEffect(
-    () => () => {
-      window.clearTimeout(animationTimeoutRef.current);
-    },
-    []
-  );
+      if (longPressTimeoutRef.current) {
+        clearTimeout(longPressTimeoutRef.current);
+      }
+    }, [withLongPress]);
 
-  useEffect(() => {
-    if (!targetRef.current) {
-      return;
-    }
+    useEffect(
+      () => () => {
+        window.clearTimeout(animationTimeoutRef.current);
+      },
+      []
+    );
 
-    const controller = new AbortController();
+    useEffect(() => {
+      if (!withLongPress) {
+        return;
+      }
 
-    // Prevent selecting the element on long press
-    targetRef.current.style.userSelect = 'none';
-    targetRef.current.style.webkitUserSelect = 'none';
+      if (!targetRef.current) {
+        return;
+      }
 
-    // Handle the long press
-    targetRef.current.addEventListener('pointerdown', onPointerDown, { signal: controller.signal });
-    targetRef.current.addEventListener('pointerup', onPointerUp, { signal: controller.signal });
-    targetRef.current.addEventListener('pointercancel', onPointerCancel, { signal: controller.signal });
+      const controller = new AbortController();
 
-    return () => {
-      controller.abort();
-    };
-  }, [onPointerCancel, onPointerDown, onPointerUp]);
+      // Prevent selecting the element on long press
+      targetRef.current.style.userSelect = 'none';
+      targetRef.current.style.webkitUserSelect = 'none';
 
-  useEventListener('contextmenu', onContextMenu);
+      // Handle the long press
+      targetRef.current.addEventListener('pointerdown', onPointerDown, { signal: controller.signal });
+      targetRef.current.addEventListener('pointerup', onPointerUp, { signal: controller.signal });
+      targetRef.current.addEventListener('pointercancel', onPointerCancel, { signal: controller.signal });
+      targetRef.current.addEventListener('pointermove', onPointerMove, { signal: controller.signal });
 
-  return (
-    <>
-      {props.children(targetRef)}
-      <Portal {...props.portalProps}>
-        <Menu isLazy isOpen={isOpen} gutter={0} placement="auto-end" onClose={onClose} {...props.menuProps}>
-          <MenuButton
-            aria-hidden={true}
-            w={1}
-            h={1}
-            position="absolute"
-            left={position[0]}
-            top={position[1]}
-            cursor="default"
-            bg="transparent"
-            size="sm"
-            _hover={_hover}
-            pointerEvents="none"
-            {...props.menuButtonProps}
-          />
-          {props.renderMenu()}
-        </Menu>
-      </Portal>
-    </>
-  );
-});
+      return () => {
+        controller.abort();
+      };
+    }, [onPointerCancel, onPointerDown, onPointerMove, onPointerUp, withLongPress]);
+
+    useEventListener('contextmenu', onContextMenu);
+
+    return (
+      <>
+        {children(targetRef)}
+        <Portal {...portalProps}>
+          <Menu isLazy isOpen={isOpen} gutter={0} placement="auto-end" onClose={onClose} {...menuProps}>
+            <MenuButton
+              aria-hidden={true}
+              w={1}
+              h={1}
+              position="absolute"
+              left={position[0]}
+              top={position[1]}
+              cursor="default"
+              bg="transparent"
+              size="sm"
+              _hover={_hover}
+              pointerEvents="none"
+              {...menuButtonProps}
+            />
+            {renderMenu()}
+          </Menu>
+        </Portal>
+      </>
+    );
+  }
+);
 
 const _hover: ChakraProps['_hover'] = { bg: 'transparent' };
