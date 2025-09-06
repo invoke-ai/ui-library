1302c2eb39167c3bae5a6fdfd8a862fbd295dea7
Attila Cseh <attila.cseh@outlook.hu>
2025-09-06 12:31:37 +0200

    storybook packages fixed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,9 +4,7 @@ const config: StorybookConfig = {
   stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
